50f1e3ce2d1bfe8a25d5ff8d27a0d855392d32f6
Andy Ju <andy.ju@duke.edu>
2018-10-30 00:29:22 -0400

    lex parser coming together: error display, functions initialized

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -7,46 +7,108 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 
 export class EditorComponent implements OnInit {
-  @ViewChild('sCanvas') sCanvas : ElementRef;
+  @ViewChild('sCanvas') sCanvas: ElementRef;
   //potentially problematic
-  private context : CanvasRenderingContext2D;
-  private editorVal: string = "";
-  private userInput = "";
+  private context: CanvasRenderingContext2D;
+  private userInput: string = "";
+  private errorOut: string = "";
+  private color: string;
+  //IMPORTANT: SAVE OBJECTS IN DATA STRUCTURES
 
-
+  //CONSTRCTOR & INIT
   constructor() { }
-
   ngOnInit() {
     this.context = (this.sCanvas.nativeElement as HTMLCanvasElement).getContext('2d');
   }
 
-  onSubmit(){
+  //PARSING, ETC.
+  private readFunc(f: string) {
+    if (f != undefined || f.length > 0) {
+      var func: string[] = f.split(" ");
+      var call: string = func[0];
 
+      //draw primitive shapes
+      if(call.match(/^(circle|line|text|rectange|oval|polygon)$/)){
+        this.drawShape(func);
+      //draw data structures
+      }else if(call.match(/^(array|queue|stack|list|listpointer|tree|graph|node|marker)$/)){
+        this.drawDataStruct(func);
+      //use commands that target a structure or shape
+      }else if(call.match(/^(changeParam|moveRelative|scale|remove|push|pop|enqueue|dequeue|moveMarker)$/)){
+        this.targetCommands(func);
+      //use commands that regulate rendering (excluding connectNodes)
+      }else if(call.match(/^(begin|delay|groupObject|connectNodes)$/)){
+        this.otherCommands(func);
+      }else {
+        this.color = "red";
+        this.errorOut = "ERROR: Call to command, " + call + ", could not be found!"
+      }
+    }
+  }
+  //function to draw primitive shapes
+  private drawShape(func : string[]) {
+    var call: string = func[0];
+
+    if (call == "circle") {
+    } else if (call == "line") {
+
+    } else if (call == "text") {
+
+    } else if (call == "rectangle") {
+
+    } else if (call == "oval") {
+
+    } else if (call == "polygon") {
+
+    }
+  }
+
+  //function to render data structure
+  private drawDataStruct(func: string[]) {
 
   }
 
-  private clearCanvas(){
-    this.context.clearRect(0, 0, (this.sCanvas.nativeElement as HTMLCanvasElement).width, (this.sCanvas.nativeElement as HTMLCanvasElement).height);
+  //function to target and modify a current structures
+  private targetCommands(func: string[]){
+
   }
+
+  //function for all other functions (connect nodes excluded, most are grouping and regulatory)
+  private otherCommands(func: string[]){
+
+  }
+
+
 
   private drawCircle() {
-    this.context.beginPath();
-    this.context.arc(100, 100, 10, 0, 2 * Math.PI, false);
-    this.context.fillStyle = 'green';
-    this.context.fill();
-    this.context.lineWidth = 1;
-    this.context.strokeStyle = '#003300';
-    this.context.stroke();
+  }
 
-    this.context.beginPath();
-    this.context.arc(100, 50, 5, 0, 2 * Math.PI, false);
-    this.context.fillStyle = 'blue';
-    this.context.fill();
-    this.context.lineWidth = 1;
-    this.context.strokeStyle = '#003300';
-    this.context.stroke();
+  //modifyCircle
+  private modifyCircle() {
+
+  }
+
+  //Handle errors if found
+  private errorHandler() {
+
   }
 
 
+  onSubmit() {
+    var functions: string[] = this.userInput.split("\n");
+    for (var i = 0; i < functions.length; i++) {
+      this.readFunc(functions[i]);
+    }
+    //if everything worked
+    if(this.color != "red"){
+      this.color = "green";
+      this.errorOut = "SUCCESS: code ran without any noticeable errors!"
+    }
 
+
+  }
+
+  clearCanvas() {
+    this.context.clearRect(0, 0, (this.sCanvas.nativeElement as HTMLCanvasElement).width, (this.sCanvas.nativeElement as HTMLCanvasElement).height);
+  }
 }
