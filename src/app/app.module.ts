3ccff78b0c0e476e5a3f1f04d2ee2fddc1a67294
Andy Ju <andy.ju@duke.edu>
2018-10-29 21:17:12 -0400

    canvas element and print function added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
 import { AceEditorModule} from 'ng2-ace-editor';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AceEditorModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,33 +1,52 @@
-import {Component, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector:'app-editor',
-  templateUrl: './editor.component.html'
-
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent {
-  @ViewChild('editor') editor;
-  text: string="";
 
-  userInput="";
+export class EditorComponent implements OnInit {
+  @ViewChild('sCanvas') sCanvas : ElementRef;
+  //potentially problematic
+  private context : CanvasRenderingContext2D;
+  private editorVal: string = "";
+  private userInput = "";
 
-  ngAfterViewInit() {
-       this.editor.setTheme("eclipse");
 
-       this.editor.getEditor().setOptions({
-           enableBasicAutocompletion: true
-       });
+  constructor() { }
 
-       this.editor.getEditor().commands.addCommand({
-           name: "showOtherCompletions",
-           bindKey: "Ctrl-.",
-           exec: function (editor) {
-
-           }
-       })
-   }
-
-  onSubmit(editor){
-    this.userInput = this.text;
+  ngOnInit() {
+    this.context = (this.sCanvas.nativeElement as HTMLCanvasElement).getContext('2d');
   }
+
+  onSubmit(){
+
+
+  }
+
+  private clearCanvas(){
+    this.context.clearRect(0, 0, (this.sCanvas.nativeElement as HTMLCanvasElement).width, (this.sCanvas.nativeElement as HTMLCanvasElement).height);
+  }
+
+  private drawCircle() {
+    this.context.beginPath();
+    this.context.arc(100, 100, 10, 0, 2 * Math.PI, false);
+    this.context.fillStyle = 'green';
+    this.context.fill();
+    this.context.lineWidth = 1;
+    this.context.strokeStyle = '#003300';
+    this.context.stroke();
+
+    this.context.beginPath();
+    this.context.arc(100, 50, 5, 0, 2 * Math.PI, false);
+    this.context.fillStyle = 'blue';
+    this.context.fill();
+    this.context.lineWidth = 1;
+    this.context.strokeStyle = '#003300';
+    this.context.stroke();
+  }
+
+
+
 }
